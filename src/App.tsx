a7810b402fa3b05de9417b88b8664db693d32ebb
Sergio Mendoza <sergiomendozansx@gmail.com>
2024-01-12 16:19:43 +0000

    Update App.tsx and IncomeExpenses.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+// timer;;
 
 export default function App() {
   return (
@@ -8,6 +10,7 @@ export default function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
       <h2>hello</h2>
     </div>
