422897d7c4463b7f7e040e5ae4f3d56f342e385a
Sergio Mendoza <sergiomendozansx@gmail.com>
2024-01-12 16:12:05 +0000

    Update App.tsx and Balance.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles.css";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
       <h2>hello</h2>
     </div>
   );
