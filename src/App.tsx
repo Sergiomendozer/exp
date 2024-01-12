216fc5158b901b470318d667aa1e8efdb7c3b2b2
CodeSandbox <support@codesandbox.io>
2024-01-12 15:22:44 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./styles.css";
+import { Header } from "./components/Header";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <Header />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+// Header component for the Expense Tracker app
+// Text displayed as the main title of the app
+export const Header = () => {
+  return <h2>Expense Tracker</h2>;
+};
