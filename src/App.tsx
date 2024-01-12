265700ba6411efc6e07ac52ffa61a4e40150d6ec
Sergio Mendoza <sergiomendozansx@gmail.com>
2024-01-12 16:33:06 +0000

    Update App.tsx and TransactionList.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./styles.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+// import { AddTransaction } from "./components/AddTransaction";
 // timer;;
 
 export default function App() {
@@ -11,6 +13,7 @@ export default function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
